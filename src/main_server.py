036310b0925a53ee12a900f9bcb2aff9233cc92e
Krakoer <axelrousselot@outlook.fr>
2022-08-29 14:43:20 -0400

    Correct main_server

--- src/main_server.py
+++ src/main_server.py
@@ -1,11 +1,9 @@
 from client import Client
-from KGC import KGC
 from authority import Authority
 import time
 
 if __name__ == "__main__":
     debug = True
-    authority_url = "http://92.222.82.236:3000"
     # Setup LO
     authority = Authority(url="0.0.0.0")
     authority.run_server()
